499bb81d5cb17753aae83f3add89b5dd0a5f6774
hubaka ghayati setyakurnia <hubakag@gmail.com>
2026-05-11 18:09:48 +0700

    bugfix

--- Herobot/app/build.gradle.kts
+++ Herobot/app/build.gradle.kts
@@ -1,54 +1,9 @@
 plugins {
     id("com.android.application")
-    id("java") // Enables Java support
-    id("application") // Allows you to run the app directly via Gradle
-    
-
-}
-
-rootProject.name = "Herobot"
-include(":app")
-
-group = "com.herobot"
-version = "1.0"
-
-repositories {
-    mavenCentral() // Fetches your Java libraries from the central repository
-}
-
-dependencies {
-    // SQLite JDBC Driver for Java
-    implementation("org.xerial:sqlite-jdbc:3.45.1.0")
-
-    // Apache Commons Math for Java calculations
-    implementation("org.apache.commons:commons-math3:3.6.1")
-
-    // JSON library for Java
-    implementation("org.json:json:20240303")
-}
-
-application {
-    // This tells Gradle which Java class contains your 'public static void main'
-    mainClass.set("Chatbot")
-}
-
-tasks.jar {
-    archiveBaseName.set("Herobot")
-    archiveVersion.set("1.0")
-
-    manifest {
-        attributes(
-            mapOf("Main-Class" to "Chatbot")
-        )
-    }
-}
-
-tasks.named<JavaExec>("run") {
-    // This allows your Java app to accept keyboard input in the terminal [cite: 2]
-    standardInput = System.`in`
 }
 
 android {
+    namespace = "com.herobot"
     compileSdk = 33
 
     defaultConfig {
@@ -62,7 +17,17 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
+}
+
+dependencies {
+    implementation("org.xerial:sqlite-jdbc:3.45.1.0")
+    implementation("org.apache.commons:commons-math3:3.6.1")
+    implementation("org.json:json:20240303")
 }
